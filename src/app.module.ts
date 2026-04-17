ec92b70367dbe4d2ac278be54940edfab9c74e41
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2026-04-17 18:34:39 +0700

    chore: register CartModule and OrdersModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
-    AuthModule],
+    AuthModule,
+    OrdersModule,
+    CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
